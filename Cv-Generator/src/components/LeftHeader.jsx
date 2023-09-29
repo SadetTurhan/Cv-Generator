d079a7541495bedb07651c8445de87a78d4c253d
Sadet Turhan <msadetturhan1@gmail.com>
2023-09-29 19:07:06 +0300

    working on headerer

--- Cv-Generator/src/components/LeftHeader.jsx
+++ Cv-Generator/src/components/LeftHeader.jsx
@@ -1,8 +1,9 @@
 function LeftHeader() {
 
     return (
-      <div className="border-2 border-black">
-        <h1>Your Cv Generator</h1>
+      <div className="flex flex-col border-2 border-black">
+        <h1 className="text-center my-2">Your Cv Generator!</h1>
+        <h2 className="text-center"><a href="https://github.com/SadetTurhan">See my github profile</a></h2>
       </div>
     )
   }
